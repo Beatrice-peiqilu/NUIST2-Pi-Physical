cc3271667373f4e374a711e16541a540fe29595c
Beatrice-peiqilu <3159897866@qq.com>
2026-03-30 19:57:19 +0800

    Add Quiz1.py with comments

--- Quiz1.py
+++ Quiz1.py
@@ -1,7 +1,10 @@
+#Authot:Lu Peiqi
+#Date:2026/3/30
+#NUIST Quiz Game in Python
 def quiz():
 	print("Welcome to the Animal Quizi!")
 	print("Answer the following questions:")
-#
+#Questions and Answers
 	questions=[
 		"1.What is the largest animal on Earth?:a.Blue Whale,b.Mouse,c.Cat",
 		"2.Which bird can fly backwards?:a.Cuckoo,b,Eagle,c.Hummingbird",
@@ -14,7 +17,7 @@ def quiz():
 	]
 	score=0
 
-#
+#Ask questions
 	for i in range(len(questions)):
 		user_answer=input(questions[i]).strip().lower()
 		if user_answer==answers[i]:
@@ -22,9 +25,9 @@ def quiz():
 			score+=1
 		else:
 			print("Incorrect!")
-#
+#Provide final score
 	print("\nQuiz completed!")
 	print(f"You got {score}/{len(questions)}questions correct.")
 
-#
+#Run the quiz function
 quiz()
